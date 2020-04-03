a6c7257b92553c0f588d14e9d61f0e14c78e4454
Nirane <53124181+nirane@users.noreply.github.com>
2020-04-03 16:31:09 +0200

    Image Processing #1

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import cv2 as cv
 
-
 def main():
-    image = cv.imread("Photos/Hand_0000002.jpg", 0)
-    # image = cv.cvtColor(image,)
+    image = cv.imread("Photos/Hand_0000002.jpg")
 
-    thresh = cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+    img_ycbcr = cv.cvtColor(image, cv.COLOR_RGB2YCrCb)
+    Cb = img_ycbcr[1]
+    Cr = img_ycbcr[2]
 
-    # image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    # ret, thresh = cv.threshold(image, 127, 255, 0)
+    central_color = img_ycbcr[800,600]
+    print(central_color)
+    Cb_color = central_color[1]
+    Cr_color = central_color[2]
+
+    Cb_diff = 15
+    Cr_diff = 15
+
+   # rows,cols = img_ycbcr.shape
+    for i in range(1200):
+        for j in range(1600):
+            pixel = img_ycbcr[i,j]
+            if(Cb_color - Cb_diff<=pixel[1]<=Cb_color+Cb_diff and Cr_color - Cr_diff<=pixel[2]<=Cr_color+Cr_diff):
+                img_ycbcr[i,j]=255
+            else:
+                img_ycbcr[i,j]=0
 
     cv.namedWindow("Reka", cv.WINDOW_NORMAL)
     cv.resizeWindow("Reka", 800, 600)
-    cv.imshow("Reka", thresh)
-
-    # contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    '''
-    edged = cv.Canny(image, 25, 160)
-    contours, hierarchy = cv.findContours(edged,  cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-
-    cv.drawContours(image, contours, -1, (0, 255, 0), 3)
-
-    cv.namedWindow("Reka",cv.WINDOW_NORMAL)
-    cv.resizeWindow("Reka",800,600)
-    cv.imshow("Reka",image)
-    '''
+    cv.imshow("Reka", img_ycbcr)
 
     cv.waitKey(4000)
 
