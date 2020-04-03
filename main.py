fe778a3c32c3d938ee80b4ffea8833ec6267eeb3
Nirane <53124181+nirane@users.noreply.github.com>
2020-04-03 17:15:58 +0200

    Image Processing #2

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 import cv2 as cv
+import os
+import numpy as np
 
 def main():
-    image = cv.imread("Photos/Hand_0000002.jpg")
 
-    img_ycbcr = cv.cvtColor(image, cv.COLOR_RGB2YCrCb)
-    Cb = img_ycbcr[1]
-    Cr = img_ycbcr[2]
+    photos = os.listdir("Photos")
+    for photo in photos:
 
-    central_color = img_ycbcr[800,600]
-    print(central_color)
-    Cb_color = central_color[1]
-    Cr_color = central_color[2]
 
-    Cb_diff = 15
-    Cr_diff = 15
+        image = cv.imread("Photos/"+photo)
 
-   # rows,cols = img_ycbcr.shape
-    for i in range(1200):
-        for j in range(1600):
-            pixel = img_ycbcr[i,j]
-            if(Cb_color - Cb_diff<=pixel[1]<=Cb_color+Cb_diff and Cr_color - Cr_diff<=pixel[2]<=Cr_color+Cr_diff):
-                img_ycbcr[i,j]=255
-            else:
-                img_ycbcr[i,j]=0
+        height = 100
+        width =100
 
-    cv.namedWindow("Reka", cv.WINDOW_NORMAL)
-    cv.resizeWindow("Reka", 800, 600)
-    cv.imshow("Reka", img_ycbcr)
+        img_ycbcr = cv.cvtColor(image, cv.COLOR_RGB2YCrCb)
+        Cb = img_ycbcr[1]
+        Cr = img_ycbcr[2]
+        img_ycbcr = cv.resize(img_ycbcr, (height,width))
+        central_color = img_ycbcr[50,50]
 
-    cv.waitKey(4000)
+        Cb_color = central_color[1]
+        Cr_color = central_color[2]
+
+        Cb_diff = 20
+        Cr_diff = 20
+
+
+       # rows,cols = img_ycbcr.shape
+        for i in range(height):
+            for j in range(width):
+                pixel = img_ycbcr[i,j]
+                if(Cb_color - Cb_diff<=pixel[1]<=Cb_color+Cb_diff and Cr_color - Cr_diff<=pixel[2]<=Cr_color+Cr_diff):
+                    img_ycbcr[i,j]=255
+                else:
+                    img_ycbcr[i,j]=0
+
+        cv.namedWindow("Reka", cv.WINDOW_NORMAL)
+        cv.resizeWindow("Reka", 800, 600)
+        cv.imshow("Reka", img_ycbcr)
+
+        cv.waitKey(3000)
 
 
 if __name__ == "__main__":
